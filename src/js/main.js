db963caecfd6b3f39dd1e76237691d0b40def3db
Aurélien Grimaud <a.grimaud@involt.io>
2020-04-22 21:23:53 +0200

    streaming status each minute
    With reusable functions writing at root

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 //- plyr options
 
-// Plyr
-var streamUrl = 'http://s2.free-shoutcast.com:18442/?type=http&nocache=5',
+// Global
+var streamUrl = 'https://streams.radiomast.io/d18496eb-7d4a-4b80-aac1-757c7da9cb17',
     title = 'Jean-Bobby Radio',
-    toggle = false;
+    toggle = false,
+    freq = 10000,
+    cta = $('.play-cta__btn');
 
+function $(elmt) {
+  return document.querySelector(elmt)
+};
+
+function cl(elmt) {
+  return $(elmt).classList
+};
+
+// Plyr
 const player = new Plyr('.player', {
   title: title,
   controls: [
@@ -32,10 +43,10 @@ function playJB() {
 
 function check() {
   if(toggle == true) {
-    document.getElementById('check').checked = false;
+    $('#check').checked = false;
     toggle = false;
   } else {
-    document.getElementById('check').checked = true;
+    $('#check').checked = true;
     toggle = true;
   }
 };
@@ -45,15 +56,41 @@ function getStatus() {
   var req = new Request(streamUrl);
 
   fetch(req).then(function(response) {
-    console.log('on air');
-    document.querySelector('.status').classList.add('status--on-air');
+    onAir();
   })
   .catch(function() {
-    console.log('off air');
-    document.querySelector('.status').classList.add('status--off-air');
-    document.querySelector('.play-cta__btn').classList.add('play-cta__btn--unactive');
-    document.querySelector('#check').classList.add('input--unactive');
+    offAir();
   })
 };
 
-document.addEventListener('load', getStatus())
+function rlStatus() {
+  setInterval(getStatus, freq);
+};
+
+function onAir() {
+  console.log('on-air');
+
+  cl('.status').remove('status--off-air');
+  cl('.status').add('status--on-air');
+
+  cl('.play-cta__btn').remove('play-cta__btn--unactive');
+
+  cl('#check').remove('input--unactive');
+};
+
+function offAir() {
+  console.log('off-air');
+
+  cl('.status').remove('status--on-air')
+  cl('.status').add('status--off-air');
+
+  cl('.play-cta__btn').add('play-cta__btn--unactive');
+  
+  cl('#check').add('input--unactive');
+  $('#check').checked = false;
+  toggle = false;
+};
+
+document.addEventListener('load', getStatus());
+document.addEventListener('load', rlStatus());
+
