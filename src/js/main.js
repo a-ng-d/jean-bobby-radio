560fe91661ebc726089e76e12723724b4798507b
Aurélien Grimaud <a.grimaud@involt.io>
2020-04-22 23:31:39 +0200

    indentation and code formating

--- src/js/main.js
+++ src/js/main.js
@@ -2,93 +2,93 @@
 
 // Global
 var streamUrl = 'https://streams.radiomast.io/d18496eb-7d4a-4b80-aac1-757c7da9cb17',
-    title = 'Jean-Bobby Radio',
-    toggle = false,
-    freq = 10000,
-    cta = $('.play-cta__btn');
+		title = 'Jean-Bobby Radio',
+		toggle = false,
+		freq = 10000,
+		cta = $('.play-cta__btn');
 
 function $(elmt) {
-  return document.querySelector(elmt)
+	return document.querySelector(elmt)
 };
 
 function cl(elmt) {
-  return $(elmt).classList
+	return $(elmt).classList
 };
 
 // Plyr
 const player = new Plyr('.player', {
-  title: title,
-  controls: [
-    'mute',
-    'volume',
-    'airplay'
-  ]
+	title: title,
+	controls: [
+		'mute',
+		'volume',
+		'airplay'
+	]
 });
 
 player.source = {
-  type: 'audio',
-  title: 'Place du Village - Spotify',
-  sources: [
-    {
-      src: streamUrl,
-      type: 'audio/mpeg',
-    }
-  ]
+	type: 'audio',
+	title: 'Place du Village - Spotify',
+	sources: [
+		{
+			src: streamUrl,
+			type: 'audio/mpeg',
+		}
+	]
 };
 
 // Play button toggle
 function playJB() {
-  return player.togglePlay()
+	return player.togglePlay()
 };
 
 function check() {
-  if(toggle == true) {
-    $('#check').checked = false;
-    toggle = false;
-  } else {
-    $('#check').checked = true;
-    toggle = true;
-  }
+	if(toggle == true) {
+		$('#check').checked = false;
+		toggle = false;
+	} else {
+		$('#check').checked = true;
+		toggle = true;
+	}
 };
 
 // Streaming status
 function getStatus() {
-  var req = new Request(streamUrl);
+	var req = new Request(streamUrl);
 
-  fetch(req).then(function(response) {
-    onAir();
-  })
-  .catch(function() {
-    offAir();
-  })
+	fetch(req).then(function(response) {
+		onAir();
+	})
+	.catch(function() {
+		offAir();
+	})
 };
 
 function rlStatus() {
-  setInterval(getStatus, freq);
+	setInterval(getStatus, freq);
 };
 
 function onAir() {
-  console.log('on-air');
+	console.log('on-air');
 
-  cl('.status').remove('status--off-air');
-  cl('.status').add('status--on-air');
+	cl('.status').remove('status--off-air');
+	cl('.status').add('status--on-air');
 
-  cl('.play-cta__btn').remove('play-cta__btn--unactive');
+	cl('.play-cta__btn').remove('play-cta__btn--unactive');
 
-  cl('#check').remove('input--unactive');
+	cl('#check').remove('input--unactive');
 };
 
 function offAir() {
-  console.log('off-air');
+	console.log('off-air');
 
-  cl('.status').remove('status--on-air')
-  cl('.status').add('status--off-air');
+	cl('.status').remove('status--on-air')
+	cl('.status').add('status--off-air');
 
-  cl('.play-cta__btn').add('play-cta__btn--unactive');
-  
-  cl('#check').add('input--unactive');
-  $('#check').checked = false;
-  toggle = false;
+	cl('.play-cta__btn').add('play-cta__btn--unactive');
+	
+	cl('#check').add('input--unactive');
+	$('#check').checked = false;
+	toggle = false;
 };
 
 document.addEventListener('load', getStatus());
