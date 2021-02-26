691bf6ca4b2964d968a08983039806fc0786c684
Aurélien Grimaud <a.grimaud@involt.io>
2021-02-26 12:31:35 +0100

    make a difference between production and development workflow

--- webpack.config.js
+++ webpack.config.js
@@ -4,79 +4,82 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const config = {
-  mode: 'production',
-  entry: {
-    jbradio: './src/jbradio.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'static'),
-    filename: '[name].min.js',
-    assetModuleFilename: 'images/[name][ext]'
-  },
-  devServer: {
-    port: 8080,
-    compress: true,
-    contentBase: [
-      path.join(__dirname, 'static'),
-      path.join(__dirname, '/assets/images'),
-    ],
-    publicPath: '/assets/'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/templates/main.pug'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].min.css',
-      chunkFilename: '[id].min.css',
-      ignoreOrder: false
-    })
-  ],
-  module: {
-    rules: [
+const
+  sassLoader = {
+    test: /\.(sa|c)ss$/i,
+    use: [
       {
-        test: /\.(sa|c)ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options : {
-              publicPath: './assets'
-            }
-          },
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: ['autoprefixer']
-              }
-            }
-          },
-          'sass-loader'
-        ]
+        loader: MiniCssExtractPlugin.loader,
+        options : {
+          publicPath: './assets'
+        }
       },
+      'css-loader',
       {
-        test: /\.(png|svg|jpg)$/i,
-        type: 'asset/resource'
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: ['autoprefixer']
+          }
+        }
       },
-      {
-        test: /\.pug$/i,
-        use: ['pug-loader']
-      }
+      'sass-loader'
     ]
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new UglifyJsPlugin()
-    ],
+  assetLoader = {
+    test: /\.(png|svg|jpg)$/i,
+    type: 'asset/resource'
   },
-};
+  pugLoader = {
+    test: /\.pug$/i,
+    use: ['pug-loader']
+  },
+  pluginCss = new MiniCssExtractPlugin({
+    filename: '[name].min.css',
+    chunkFilename: '[id].min.css',
+    ignoreOrder: false
+  }),
+  pluginPug = new HtmlWebpackPlugin({
+    template: './src/templates/main.pug'
+  }),
+
+  config = {
+    entry: {
+      jbradio: './src/jbradio.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'static'),
+      filename: '[name].min.js',
+      assetModuleFilename: 'images/[name][ext]'
+    },
+    devServer: {
+      port: 8080,
+      compress: true,
+      contentBase: [
+        path.join(__dirname, 'static'),
+        path.join(__dirname, '/assets/images'),
+      ],
+      publicPath: '/assets/'
+    },
+    plugins: [],
+    module: {
+      rules: []
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [],
+    },
+  };
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {}
-  if (argv.mode === 'production') {}
+  if (argv.mode === 'development') {
+    config.module.rules.push(sassLoader, pugLoader);
+    config.plugins.push(pluginCss, pluginPug)
+  }
+  if (argv.mode === 'production') {
+    config.optimization.minimizer.push(new UglifyJsPlugin(), new CssMinimizerPlugin());
+    config.module.rules.push(sassLoader, assetLoader, pugLoader);
+    config.plugins.push(pluginCss, pluginPug)
+  }
   return config;
 }
