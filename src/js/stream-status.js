649ca8e8bd4afc1a9252461fb9b0d61eaec2681a
Aurélien Grimaud <a.grimaud@involt.io>
2021-04-14 21:00:30 +0200

    stop animation when off air

--- src/js/stream-status.js
+++ src/js/stream-status.js
@@ -1,14 +1,15 @@
 import { cl, playState, io } from './global';
 import { stream } from './data';
 import { player } from './player';
+import { stopAnimation } from './animation';
 
 export function onAir() {
 
 	console.log('ON AIR');
 
-	cl('.status').replace('status--off-air', 'status--on-air');
+	cl('.status').replace('status--off-air', 'status--on-air'); // status tag
 
-	cl('.play-cta__btn').remove('play-cta__btn--unactive');
+	cl('.play-cta__btn').remove('play-cta__btn--unactive'); // allow user to play
 
 	cl('input[name=\'play\']').remove('input--unactive')
 
@@ -19,15 +20,18 @@ export function offAir() {
 
 	console.log('OFF AIR');
 
-	cl('.status').replace('status--on-air', 'status--off-air')
+	cl('.status').replace('status--on-air', 'status--off-air'); // status tag
 
-	cl('.play-cta__btn').add('play-cta__btn--unactive');
+	cl('.play-cta__btn').add('play-cta__btn--unactive'); // avoid user to play
 
+	// player
 	cl('input[name=\'play\']').add('input--unactive');
 	playState.checked = false;
 	player.stop();
 
-	cl('.track').replace('track--end', 'track--start');
+	stopAnimation(); // animation loop
+
+	cl('.track').replace('track--end', 'track--start'); // hide track infos
 
 	io.setState = false
 
