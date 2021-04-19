053876c26c304e31c3671762a62d7ee4b5fc43f2
Aurélien Grimaud <a.grimaud@involt.io>
2021-04-19 15:38:33 +0200

    multiple toggle buttons behavior

--- src/js/components.js
+++ src/js/components.js
@@ -1,4 +1,4 @@
-import { $ } from './global';
+import { $, each$ } from './global';
 
 let toggle = false;
 
@@ -7,26 +7,47 @@ export const
 		notifBtn = $('.switch--now-playing'),
 		notifState = $('input[name=\'switch\']'),
 		playState = $('input[name=\'play\']'),
-    toggleBtn = $('.button__toggle');
+    toggleBtn = each$('.button__toggle');
 
-export function infosCard(elmt, callback) {
+export function infosCard(event) {
 
-  if (elmt != undefined) {
+	const card = event.path[2];
+	const target = event.target;
+
+  if (card != undefined) {
     if (toggle) {
-
-      elmt.classList.replace('icard--expanded', 'icard--collapsed')
-      toggle = false
-
+			close()
     } else {
-
-      elmt.classList.replace('icard--collapsed', 'icard--expanded')
-      callback()
-      toggle = true
-
+      open()
     }
-  }
+  };
 
-}
+	function closingRules(event) {
+
+		const className = event.target.className;
+
+		if (typeof className != 'string') {
+			return close()
+		} else if (!className.match('icard__content') && event.target != target) {
+			return close()
+		}
+
+	};
+
+	function open() {
+		card.classList.replace('icard--collapsed', 'icard--expanded');
+		window.addEventListener('click', closingRules, true);
+		toggle = true
+	};
+
+	function close() {
+		card.classList.replace('icard--expanded', 'icard--collapsed');
+		window.removeEventListener('click', closingRules, true)
+		target.checked = false;
+		toggle = false
+	}
+
+};
 
 export function refreshForm() {
   $('iframe').src = $('iframe').src
